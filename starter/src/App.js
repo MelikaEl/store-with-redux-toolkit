e3d8010e7cf3b4f43b3c08e31d53abf03d3e7ffe
MelikaEl <melika.melika8@gmail.com>
2024-09-15 13:29:55 +0430

    final change

---
+++ starter/src/App.js
@@ -0,0 +1,26 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useDispatch,useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const {cartItems}=useSelector((store)=>store.cart)
+  const {isOpen} = useSelector((store)=>store.modal)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    dispatch(calculateTotals());
+  },[cartItems]);
+
+  return (
+    <main>
+      {isOpen&&<Modal/>}
+      <Navbar/>
+      <CartContainer/>
+    </main>//The <main> tag in HTML is used to define the main content of a document. These tags specially use for the SEO of website.
+
+  ); 
+}
+export default App;
